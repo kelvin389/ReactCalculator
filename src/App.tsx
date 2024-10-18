e6b4ab76cf309b96e6ce6a0a52139c7d78a6d8c7
kelvin389 <kelvinl.leung@gmail.com>
2024-10-18 04:09:26 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const operations: string[] = ["+", "-", "*", "/"];
+    const [screenValue, setScreenValue] = useState<number>(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [prevValue, setPrevValue] = useState<number>(0);
+    const [op, setOp] = useState<string>("");
+    const [createNewNumber, setCreateNewNumber] = useState<boolean>(true);
+
+    function appendToScreen(num: number): void {
+        if (createNewNumber) {
+            setScreenValue(num);
+            setCreateNewNumber(false);
+        } else {
+            const newNum = parseInt(screenValue.toString() + num.toString());
+            setScreenValue(newNum);
+        }
+    }
+
+    function clearScreen(): void {
+        setScreenValue(0);
+    }
+
+    function clearAll(): void {
+        setPrevValue(0);
+        clearScreen();
+    }
+
+    function beginCalculation(op: string): void {
+        setOp(op);
+        setPrevValue(screenValue);
+        setCreateNewNumber(true);
+    }
+
+    function evaluate(): void {
+        let result: number;
+
+        switch (op) {
+            case "+":
+                result = prevValue + screenValue;
+                break;
+            case "-":
+                result = prevValue - screenValue;
+                break;
+            case "*":
+                result = prevValue * screenValue;
+                break;
+            case "/":
+                result = prevValue / screenValue;
+                break;
+            default:
+                result = 0;
+                console.log("error evaluating");
+                break;
+        }
+
+        setCreateNewNumber(true);
+        setScreenValue(result);
+    }
+
+    return (
+        <div>
+            <div>
+                <h1 className="screen">{screenValue}</h1>
+            </div>
+            <div>
+                <button className="square-button" onClick={clearAll}>CA</button>
+                <button className="square-button" onClick={clearScreen}>C</button>
+                <br />
+                <div className="numbers-container">
+                    {numbers.map((num) => (
+                        <button
+                            className="square-button"
+                            key={num}
+                            onClick={() => appendToScreen(num)}
+                        >
+                            {num}
+                        </button>
+                    ))}
+                </div>
+                <br />
+                {operations.map((op) => (
+                    <button className="square-button" key={op} onClick={() => beginCalculation(op)}>
+                        {op}
+                    </button>
+                ))}
+                <br />
+                <button className="square-button" onClick={evaluate}>=</button>
+            </div>
+
+            <div>
+                {prevValue}
+                <br />
+                {op}
+                <br />
+                {createNewNumber.toString()}
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
